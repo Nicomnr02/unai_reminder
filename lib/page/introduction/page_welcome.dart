ac9197375fe72b76f9bfc3e8dabcd5f150d6eb11
Nicolas Manurung <100906849+Nicomnr02@users.noreply.github.com>
2023-03-31 17:32:31 +0700

    feat: added a friendly view for permissions (autostart, notification permission)

--- lib/page/introduction/page_welcome.dart
+++ lib/page/introduction/page_welcome.dart
@@ -1,48 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:unai_reminder/page/authentication/page_authentication.dart';
+import 'package:unai_reminder/page/introduction/welcome_sub_page/welcome_autostart_permission.dart';
+import 'package:unai_reminder/page/introduction/welcome_sub_page/welcome_introduce.dart';
+import 'package:unai_reminder/page/introduction/welcome_sub_page/welcome_noti_permission.dart';
 
-class IntroductionPage extends StatelessWidget {
+class IntroductionPage extends StatefulWidget {
   const IntroductionPage({super.key});
+
+  @override
+  State<IntroductionPage> createState() => _IntroductionPageState();
+}
+
+class _IntroductionPageState extends State<IntroductionPage> {
+
+  // declare and initizlize the page controller
+  final PageController _pageController = PageController(initialPage: 0);
+
+  // the index of the current page
+  int _activePage = 0;
+
+  // this list holds all the pages
+  final List<Widget> _pages = [
+    const WelcomeIntroduce(),
+    const WelcomeAutostartPermission(),
+    const WelcomeNotificationPermission()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
+      body: Stack(
+        children: [
+          // the page view
+          PageView.builder(
+            controller: _pageController,
+            onPageChanged: (int page) {
+              setState(() {
+                _activePage = page;
+              });
+            },
+            itemCount: _pages.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _pages[index % _pages.length];
+            },
+          ),
+          // Display the dots indicator
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List<Widget>.generate(
+                  _pages.length,
+                  (index) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: InkWell(
+                          onTap: () {
+                            _pageController.animateToPage(index,
+                                duration: const Duration(milliseconds: 50),
+                                curve: Curves.easeIn);
+                          },
+                          child: CircleAvatar(
+                            radius: 8,
+                            // check if a dot is connected to the current page
+                            // if true, give it a different color
+                            backgroundColor: _activePage == index
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                        ),
+                      )),
             ),
-            Image.asset('assets/images/times.png', width: 250),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              "Get On Schedule.\nGet Productive.",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "Sp",
-                  fontWeight: FontWeight.w600,
-                  fontSize: 40,
-                  fontStyle: FontStyle.italic),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ));
-                },
-                child: const Text(
-                  "Login Now",
-                  style: TextStyle(fontFamily: 'Sp'),
-                ))
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
